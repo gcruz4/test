210bdd30949cc8f4dbba4ffc757761f833ee2258
gcruz4 <43196718+gcruz4@users.noreply.github.com>
2018-11-01 09:57:32 -0700

    Add files via upload

---
+++ program.js
@@ -0,0 +1,13 @@
+﻿function allnumeric(inputtxt) {
+    var numbers = /^[5-20]+$/;
+    if (inputtxt.value.match(numbers)) {
+        alert('Your number has accepted....');
+        document.form1.text1.focus();
+        return true;
+    }
+    else {
+        alert('the valid input is a number between 5 and 20, please refresh your page to start again');
+        document.form1.text1.focus();
+        return false;
+    }
+} 
